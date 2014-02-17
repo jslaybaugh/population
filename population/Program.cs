e1de96f89749527dfa19bf0a265e89f2e78f1ad1
Jorin Slaybaugh <jslaybaugh@gmail.com>
2014-02-16 21:13:52 -0600

    testing something

--- population/Program.cs
+++ population/Program.cs
@@ -42,77 +42,63 @@ namespace population
 				Age = 0;
 			}
 
-			private int _reproductiveYearCounter = 0;
 
-			public void LiveAYear()
-			{
-				if (IsFemale)
-				{
-					if (Age >= _ReproductiveStartAge && Age < _ReproductiveEndAge && WillReproduce)
-					{
-						// female planning on reproducing within reproductive years
-						_reproductiveYearCounter += 1;
-						if (_reproductiveYearCounter % _YearsBetweenChildren == 0)
-						{
-							// have a baby
-							_NewPeopleQuantity += 1;
-							_reproductiveYearCounter = 0;
-						}
-					}
-				}
+			//public void LiveAYear()
+			//{
+			//	if (IsFemale)
+			//	{
+			//		if (Age >= _ReproductiveStartAge && Age < _ReproductiveEndAge && WillReproduce)
+			//		{
 
-				if (Age >= _AverageLifeSpan)
-				{
-					// die
-					_Dead.Add(this.Id);
-				}
+			//			if ( % _YearsBetweenChildren == 0)
+			//			{
+			//				// have a baby
+			//				_NewPeopleQuantity += 1;
+			//				ReproductiveYearCounter = 0;
+			//			}
+			//		}
+			//	}
 
-				Age += 1;
-			}
+			//	if (Age >= _AverageLifeSpan)
+			//	{
+			//		// die
+			//		_Dead.Add(this.Id);
+			//	}
+
+			//	Age += 1;
+			//}
 		}
 
 		class LifeProcess
 		{
-			public void DoWork()
-			{
-				foreach (var person in _People)
-				{
-					person.LiveAYear();
-				}
-			}
-		}
-
-		class DeathProcess
-		{
-			public void DoWork()
-			{
-				foreach (var id in _Dead)
-				{
-					_People.Remove(_People.FirstOrDefault(x => x.Id == id));
-				}
-
-				_Dead = new List<Guid>();
-			}
-		}
-
-		class BirthProcess
-		{
 			private int _thisYear;
 
-			public BirthProcess(int thisYear)
+			public LifeProcess(int thisYear)
 			{
 				_thisYear = thisYear;
 			}
+
 			public void DoWork()
 			{
-				for (int j = 0; j < _NewPeopleQuantity; j++)
-				{
-					_People.Add(new Person(_thisYear, null, null));
-				}
+
+				_People.RemoveAll(x=> _People.Where(y=>y.Age >= _AverageLifeSpan).Select(y=>y.Id).Contains(x.Id));
+					
+				//viable is who's left
+
+				_NewPeopleQuantity = _People
+					.Where(x => x.IsFemale && x.WillReproduce)
+					.Where(x => x.Age >= _ReproductiveStartAge && x.Age < _ReproductiveEndAge)
+					.Where(x => (x.Age - _ReproductiveStartAge) % _YearsBetweenChildren == 0)
+					.Count();
+
+				_People.AddRange(Enumerable.Range(1, _NewPeopleQuantity).Select(x => new Person(_thisYear, null, null)));
 
 				_NewPeopleQuantity = 0;
+
+				_People.ForEach(x => x.Age += 1);
 			}
 		}
+
 
 		private static List<Person> _People;
 		private static int _NewPeopleQuantity;
@@ -156,34 +142,13 @@ namespace population
 			{
 				Console.WriteLine("Year {0}: {1} People", i, _People.Count);
 
-				var life = new LifeProcess();
+				var life = new LifeProcess(i);
 				var lifeThread = new Thread(new ThreadStart(life.DoWork));
 
 				lifeThread.Start();
 				while (!lifeThread.IsAlive) ;
 
 				lifeThread.Join();
-
-				////////////
-
-				var birth = new BirthProcess(i);
-				var birthThread = new Thread(new ThreadStart(birth.DoWork));
-
-				birthThread.Start();
-				while (!birthThread.IsAlive);
-
-				birthThread.Join();
-
-				////////////
-
-				var death = new DeathProcess();
-				var deathThread = new Thread(new ThreadStart(death.DoWork));
-
-				deathThread.Start();
-				while (!deathThread.IsAlive);
-
-				deathThread.Join();
-
 			}
 
 			Console.WriteLine("Year {0}: {1} People", _EndYear, _People.Count);
