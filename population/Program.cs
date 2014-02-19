9fce2b76adc8ba4a97c095f9a63f6ce0f6b568db
Jorin Slaybaugh <jslaybaugh@gmail.com>
2014-02-19 00:09:04 -0600

    cool stats, but got issues

--- population/Program.cs
+++ population/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TestSimpleRNG;
 
 namespace population
 {
@@ -22,27 +23,40 @@ namespace population
 	}
 	class Program
 	{
-		private static int _InitialPopulationMale = 0;
-		private static int _InitialPopulationFemale = 0;
-		private static int _AverageLifeSpan = 0;
-		private static int _ReproductiveStartAge = 0;
-		private static int _ReproductiveEndAge = 0;
-		private static int _YearsBetweenChildren = 0;
-		private static int _PercentReproductiveFemales = 0;
-		private static int _EndYear = 0;
+		private static int _InitialPopulationMale;
+		private static int _InitialPopulationFemale;
+
+		private static int _MaxLifeSpan = 1000;
+
+		private static int _MinimumReproductiveAge;
+		private static int _MaximumReproductiveAge;
+
+		private static int _MinimumYearsBetweenChildren = 1;
+		private static int _AverageYearsBetweenChildren;
+		private static int _StdDevYearsBetweenChildren;
+
+		private static int _PercentReproductiveFemales;
+
+		private static int _EndYear;
+		private static int _CurrentYear;
+
+		//////////////////
 
 		private static Random rnd;
 
 		class Person
 		{
+			public Guid Id { get; set; }
 			public int BirthYear { get; set; }
 			public bool IsFemale { get; set; }
-			public int Age { get; set; }
 			public bool WillReproduce { get; set; }
 
-			public Guid Id { get; set; }
+			private int _LifeSpan;
+			private int _ReproductiveStartAge = -1;
+			private int _ReproductiveEndAge = -1;
+			private int _AgeAtNextBirth;
 
-			public Person(int birthYear, bool? isFemale, bool? willReproduce)
+			public Person(int birthYear, bool? isFemale, bool? willReproduce, bool fullLife)
 			{
 				Id = Guid.NewGuid();
 				BirthYear = birthYear;
@@ -51,154 +65,155 @@ namespace population
 				if (IsFemale)
 				{
 					WillReproduce = willReproduce ?? rnd.Next(0, 999) < (_PercentReproductiveFemales * 1000);
+
+					if (WillReproduce)
+					{
+						_ReproductiveStartAge = (int)(SimpleRNG.GetGamma(2, 0.5) * (_MaximumReproductiveAge - _MinimumReproductiveAge)) + _MinimumReproductiveAge;
+						_ReproductiveEndAge = (int)(SimpleRNG.GetBeta(5, 2) * (_MaximumReproductiveAge - _MinimumReproductiveAge)) + _MinimumReproductiveAge;
+						_AgeAtNextBirth = _ReproductiveStartAge;
+					}
 				}
-				Age = 0;
+
+				_LifeSpan = fullLife ? _MaxLifeSpan : (int)(SimpleRNG.GetBeta(5, 2) * _MaxLifeSpan);
+
+				Console.WriteLine("Life: {0}, ReproStart: {1}, ReproEnd: {2}", _LifeSpan, _ReproductiveStartAge, _ReproductiveEndAge);
+			}
+
+			public int Age
+			{
+				get
+				{
+					return _CurrentYear - BirthYear;
+				}
+			}
+
+			public bool IsAlive
+			{
+				get
+				{
+					return Age < _LifeSpan;
+				}
+			}
+
+			public bool IsReproductive
+			{
+				get
+				{
+					return IsAlive && _ReproductiveStartAge != -1 && (Age >= _ReproductiveStartAge && Age < _ReproductiveEndAge);
+				}
 			}
 
 
-			//public void LiveAYear()
-			//{
-			//	if (IsFemale)
-			//	{
-			//		if (Age >= _ReproductiveStartAge && Age < _ReproductiveEndAge && WillReproduce)
-			//		{
-
-			//			if ( % _YearsBetweenChildren == 0)
-			//			{
-			//				// have a baby
-			//				_NewPeopleQuantity += 1;
-			//				ReproductiveYearCounter = 0;
-			//			}
-			//		}
-			//	}
-
-			//	if (Age >= _AverageLifeSpan)
-			//	{
-			//		// die
-			//		_Dead.Add(this.Id);
-			//	}
-
-			//	Age += 1;
-			//}
-		}
-
-		class LifeProcess
-		{
-			private int _thisYear;
-
-			public LifeProcess(int thisYear)
+			public bool IsMakinABaby
 			{
-				_thisYear = thisYear;
-			}
+				get
+				{
+					if (IsReproductive && Age == _AgeAtNextBirth)
+					{
+						var gap = SimpleRNG.GetNormal(_AverageYearsBetweenChildren, _StdDevYearsBetweenChildren);
+						if (gap < 1) gap = 1;
+						_AgeAtNextBirth = Age + (int)gap;
+						return true;
+					}
 
-			public void DoWork()
-			{
-
-				_People.RemoveAll(x => _People.Where(y => y.Age >= _AverageLifeSpan).Select(y => y.Id).Contains(x.Id));
-
-				//viable is who's left
-
-				_NewPeopleQuantity = _People
-					.Where(x => x.IsFemale && x.WillReproduce)
-					.Where(x => x.Age >= _ReproductiveStartAge && x.Age < _ReproductiveEndAge)
-					.Where(x => (x.Age - _ReproductiveStartAge) % _YearsBetweenChildren == 0)
-					.Count();
-
-				_People.AddRange(Enumerable.Range(1, _NewPeopleQuantity).Select(x => new Person(_thisYear, null, null)));
-
-
-				_People.ForEach(x => x.Age += 1);
+					return false;
+				}
 			}
 		}
 
 
 		private static List<Person> _People;
-		private static int _NewPeopleQuantity;
-		private static List<Guid> _Dead;
 
 		static void Main(string[] args)
 		{
+
+			InitAndRun();
+
+
+
+		}
+
+		private static void InitAndRun()
+		{
+			Console.Clear();
+
 			Console.WriteLine("Initial Population (Males): ");
 			_InitialPopulationMale = Convert.ToInt16(Console.ReadLine());
+
 			Console.WriteLine("Initial Population (Females): ");
 			_InitialPopulationFemale = Convert.ToInt16(Console.ReadLine());
-			Console.WriteLine("Average Life Span: ");
-			_AverageLifeSpan = Convert.ToInt16(Console.ReadLine());
-			Console.WriteLine("Reproductive Start Age: ");
-			_ReproductiveStartAge = Convert.ToInt16(Console.ReadLine());
-			Console.WriteLine("Reproductive End Age: ");
-			_ReproductiveEndAge = Convert.ToInt16(Console.ReadLine());
-			Console.WriteLine("Years Between Children: ");
-			_YearsBetweenChildren = Convert.ToInt16(Console.ReadLine());
-			Console.WriteLine("Percentage of Reproductive Females: ");
+
+			Console.WriteLine("Maximum Life Span: ");
+			_MaxLifeSpan = Convert.ToInt16(Console.ReadLine());
+
+			Console.WriteLine("Minimum Reproductive Age (<= {0}): ", _MaxLifeSpan);
+			_MinimumReproductiveAge = Convert.ToInt16(Console.ReadLine());
+
+			Console.WriteLine("Maximum Reproductive Age (<= {0}): ", _MaxLifeSpan);
+			_MaximumReproductiveAge = Convert.ToInt16(Console.ReadLine());
+
+			Console.WriteLine("Average Years Between Children (between {0} and {1}): ", _MinimumYearsBetweenChildren, _MaximumReproductiveAge - _MinimumReproductiveAge);
+			_AverageYearsBetweenChildren = Convert.ToInt16(Console.ReadLine());
+
+			Console.WriteLine("Std Deviation Years Between Children (ex: 2 would mean 68% happen between {0} and {1} years): ", _AverageYearsBetweenChildren - 2, _AverageYearsBetweenChildren + 2);
+			_StdDevYearsBetweenChildren = Convert.ToInt16(Console.ReadLine());
+
+			Console.WriteLine("Percentage of Reproductive Females (between 0 and 100): ");
 			_PercentReproductiveFemales = Convert.ToInt16(Console.ReadLine());
+
 			Console.WriteLine("End Year: ");
 			_EndYear = Convert.ToInt16(Console.ReadLine());
 
-			_People = new List<Person>();
-			_Dead = new List<Guid>();
+			RunNumbers();
+		}
 
+		private static void RunNumbers()
+		{
+			_People = new List<Person>();
 			rnd = new Random();
 
-			var stp = new Stopwatch();
-			stp.Start();
+			//var stp = new Stopwatch();
+			//stp.Start();
 
 			for (int i = 0; i < _InitialPopulationMale; i++)
 			{
-				_People.Add(new Person(0, false, false));
+				_People.Add(new Person(0, false, false, true));
 			}
 
 			for (int i = 0; i < _InitialPopulationFemale; i++)
 			{
-				_People.Add(new Person(0, true, true));
+				_People.Add(new Person(0, true, true, true));
 			}
 
 			for (int i = 0; i < _EndYear; i++)
 			{
-				var init = stp.ElapsedMilliseconds;
-				Console.WriteLine("Year {0}: {1} People", i, _People.Count(x => x.Age < _AverageLifeSpan));
-				//Console.WriteLine("YEAR {0}", i);
-				//var life = new LifeProcess(i);
-				//var lifeThread = new Thread(new ThreadStart(life.DoWork));
+				_CurrentYear = i;
+				Console.WriteLine("Year {0}: {1} People", i, _People.Count(x => x.IsAlive).ToString("#,##0"));
+				long newPeeps = _People.Count(x => x.IsMakinABaby);
+				_People.AddRange(Enumerable.Range(1, newPeeps).Select(x => new Person(i, null, null, false)));
 
-				//lifeThread.Start();
-				//while (!lifeThread.IsAlive) ;
-
-				//lifeThread.Join();
-
-				var a = stp.ElapsedMilliseconds;
-				Console.WriteLine("A:{0}", a - init);
-				//_People.RemoveAll(x => _People.Where(y => y.Age >= _AverageLifeSpan).Select(y => y.Id).Contains(x.Id));
-
-				//viable is who's left
-
-				var b = stp.ElapsedMilliseconds;
-				Console.WriteLine("B:{0}", b - a);
-				long newPeeps = _People
-					.Where(x => x.Age < _AverageLifeSpan && x.IsFemale && x.WillReproduce)
-					.Where(x => x.Age >= _ReproductiveStartAge && x.Age < _ReproductiveEndAge)
-					.Where(x => (x.Age - _ReproductiveStartAge) % _YearsBetweenChildren == 0)
-					.Count();
-
-				var c = stp.ElapsedMilliseconds;
-				Console.WriteLine("C:{0}", c - b);
-				_People.AddRange(Enumerable.Range(1, newPeeps).Select(x => new Person(i, null, null)));
-
-
-				var d = stp.ElapsedMilliseconds;
-				Console.WriteLine("D:{0}", d - c);
-				_People.ForEach(x => x.Age += (x.Age < _AverageLifeSpan ? 1 : 0));
-
-
-				var e = stp.ElapsedMilliseconds;
-				Console.WriteLine("E:{0}", e - d);
 			}
 
-			Console.WriteLine("Year {0}: {1} People", _EndYear, _People.Count);
+			Console.WriteLine("Year {0}: {1} People", _EndYear, _People.Count(x => x.IsAlive).ToString("#,##0"));
 			Console.WriteLine("-----");
 			Console.WriteLine("DONE");
 			Console.WriteLine("-----");
-			Console.ReadLine();
+			Console.WriteLine("What now? ([R]epeat, [S]tart over, [Q]uit");
+			Console.Beep();
+			var action = Console.ReadLine();
+
+			if (action.ToUpper() == "REPEAT" || action.ToUpper() == "R")
+			{
+				RunNumbers();
+			}
+			else if (action.ToUpper() == "START OVER" || action.ToUpper() == "S")
+			{
+				InitAndRun();
+			}
+			else
+			{
+				// quit
+			}
 		}
 	}
 }
